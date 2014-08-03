1bd9c9f71bb3ea7ebf9cb53d980a5a68207c530f
pekastel <pablo.adrian.castillo@gmail.com>
2014-08-03 03:39:25 -0300

    wp-cli recipe fixed

--- site-cookbooks/app/recipes/wp-cli.rb
+++ site-cookbooks/app/recipes/wp-cli.rb
@@ -1,12 +1,12 @@
-# Configure WP-CLI
-bash "wp-cli-conf" do
-  code <<-EOH
-    mkdir /home/vagrant/.wp-cli
-  EOH
+directory "/home/vagrant/.wp-cli" do
+  owner "vagrant"
+  group "vagrant"
+  mode 00755
+  action :create
 end
 
 cookbook_file "/home/vagrant/.wp-cli/config.yml" do
-  source "config.yml"
+  source "wp-cli-config.yml"
   mode 0644
   owner "vagrant"
   group "vagrant"
